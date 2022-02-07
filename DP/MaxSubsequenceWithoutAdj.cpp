30aa968bebc1c3e545ee7b9c81f1a74716245a2f
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-02-08 01:49:10 +0530

    Update MaxSubsequenceWithoutAdj.cpp

--- DP/MaxSubsequenceWithoutAdj.cpp
+++ DP/MaxSubsequenceWithoutAdj.cpp
@@ -1,3 +1,5 @@
+//Memoization
+//Time-O(n) Space-O(n)+O(n)
 int f(int ind,vector<int>& nums, vector<int>& dp){
     if(ind==0)
         return dp[ind]=nums[ind];
@@ -13,4 +15,24 @@ int maximumNonAdjacentSum(vector<int> &nums){
     // Write your code here.
     vector<int>dp(nums.size(),-1);
     return f(nums.size()-1,nums,dp);
+}
+
+
+//Tabulation
+//Time-O(n) Space-O(1)
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n=nums.size();
+    int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int take=nums[i];
+        if(i>1)
+            take+=prev2;
+        int nonTake=prev;
+        int curr=max(take,nonTake);
+        prev2=prev;
+        prev=curr;
+    }
+    return prev;
 }

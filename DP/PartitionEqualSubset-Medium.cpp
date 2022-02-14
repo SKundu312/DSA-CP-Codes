cbea78af8af8588d51205e6172c349b6912a9e02
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-02-15 01:47:28 +0530

    Update PartitionEqualSubset-Medium.cpp

--- DP/PartitionEqualSubset-Medium.cpp
+++ DP/PartitionEqualSubset-Medium.cpp
@@ -1,1 +1,31 @@
-..
+//Memoization
+bool f(int ind,int target,vector<int> &arr,vector<vector<bool>> &dp ){
+    if(target==0)
+        return true;
+    
+    if(ind == 0)
+        return arr[0] == target;
+    
+    if(dp[ind][target]!=false)
+        return dp[ind][target];
+        
+    bool notTaken = f(ind-1,target,arr,dp);
+    
+    bool taken = false;
+    if(arr[ind]<=target)
+        taken = f(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken||taken;
+}
+bool canPartition(vector<int> &arr, int n)
+{
+	// Write your code here.
+    int s=0;
+    for(int i=0;i<n;i++)
+        s+=arr[i];
+    if(s%2) return false;
+    int k=s/2;
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    return f(n-1,k,arr,dp);
+}
+
